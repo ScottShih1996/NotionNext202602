9055fa1de0f02e7865c88c595e0186c84f47655f
Scott Shih <1365809+ScottShih1996@users.noreply.github.com>
2026-02-11 10:07:51 +0800

    Add SearchButton component to Header

--- themes/commerce/components/Header.js
+++ themes/commerce/components/Header.js
@@ -7,6 +7,7 @@ import CONFIG from '../config'
 import LogoBar from './LogoBar'
 import { MenuBarMobile } from './MenuBarMobile'
 import { MenuItemDrop } from './MenuItemDrop'
+import SearchButton from './SearchButton'
 
 /**
  * 顶部导航栏 + 菜单
@@ -20,6 +21,15 @@ export default function Header(props) {
 
   const { locale } = useGlobal()
 
+  // 展示搜索框
+  const toggleShowSearchInput = () => {
+    if (siteConfig('ALGOLIA_APP_ID')) {
+      searchModal.current.openSearch()
+    } else {
+      changeShowSearchInput(!showSearchInput)
+    }
+  }
+  
   const defaultLinks = [
     {
       icon: 'fas fa-th',
@@ -38,12 +48,6 @@ export default function Header(props) {
       name: locale.NAV.ARCHIVE,
       href: '/archive',
       show: CONFIG.MENU_ARCHIVE
-    },
-    {
-      icon: 'fas fa-search',
-      name: locale.NAV.SEARCH,
-      href: '/search',
-      show: CONFIG.MENU_SEARCH
     }
   ]
 
@@ -70,9 +74,9 @@ export default function Header(props) {
 
     const narrowNav = scrollS > 50
     if (narrowNav) {
-      nav && nav.classList.replace('h-24', 'h-14')
+      nav && nav.classList.replace('h-20', 'h-14')
     } else {
-      nav && nav.classList.replace('h-14', 'h-24')
+      nav && nav.classList.replace('h-14', 'h-20')
     }
   }, throttleMs)
 
@@ -94,12 +98,13 @@ export default function Header(props) {
       {/* 导航栏菜单内容 */}
       <div
         id='top-navbar'
-        className='px-4 flex w-full mx-auto max-w-screen-xl h-24 transition-all duration-200 items-between'>
+        className='px-4 flex w-full mx-auto max-w-screen-xl h-20 transition-all duration-200 items-between'>
         {/* 左侧图标Logo */}
         <LogoBar {...props} />
 
         {/* 移动端折叠按钮 */}
         <div className='mr-1 flex md:hidden justify-end items-center text-lg space-x-4 font-serif dark:text-gray-200'>
+          <SearchButton {...props} />
           <div onClick={toggleMenuOpen} className='cursor-pointer'>
             {isOpen ? (
               <i className='fas fa-times' />
@@ -113,6 +118,7 @@ export default function Header(props) {
         <div className='hidden md:flex items-center'>
           {links &&
             links?.map(link => <MenuItemDrop key={link?.id} link={link} />)}
+          <SearchButton {...props} />
         </div>
       </div>
 
