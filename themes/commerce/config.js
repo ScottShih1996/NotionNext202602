1b714632bbe60aa1d8ce16ae5f9eda02d11beda1
Scott Shih <1365809+ScottShih1996@users.noreply.github.com>
2026-02-11 12:09:19 +0800

    Update config.js

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -2,22 +2,21 @@ const CONFIG = {
   // 封面大图
   COMMERCE_HOME_BANNER_ENABLE: true,
 
-  COMMERCE_TEXT_CENTER_TITLE: 'Product Center', // 中间产品区块标题
-  COMMERCE_TEXT_CENTER_DESCRIPTION:
-    'The vision of NotionNext is to help you effortlessly and seamlessly build your own website, amplifying the value of your brand.', // 中间产品区块文字描述
-  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: 'Product Categories', // 左侧产品分类标题
-  COMMERCE_TEXT_FOOTER_TITLE: 'Contact US', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
-  COMMERCE_TEXT_FOOTER_MENU_1: 'Product Center', // COMMERCE主题页脚左侧菜单标题1
-  COMMERCE_TEXT_FOOTER_MENU_2: 'About US', // COMMERCE主题页脚左侧菜单标题2
+  COMMERCE_TEXT_CENTER_TITLE: null, // 中间产品区块标题
+  COMMERCE_TEXT_CENTER_DESCRIPTION: null, // 中间产品区块文字描述
+  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: '文章分類', // 左侧产品分类标题
+  COMMERCE_TEXT_FOOTER_TITLE: '意見回饋', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
+  COMMERCE_TEXT_FOOTER_MENU_1: '文章分類', // COMMERCE主题页脚左侧菜单标题1
+  COMMERCE_TEXT_FOOTER_MENU_2: '手冊地圖', // COMMERCE主题页脚左侧菜单标题2
 
-  COMMERCE_FOOTER_RIGHT_IMG_URL: null, // 显示页脚右侧的图片，通常放二维码
-  COMMERCE_FOOTER_RIGHT_TEXT: null, // 页脚右侧图片下的文字描述
+  COMMERCE_FOOTER_RIGHT_IMG_URL: 'https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F58a77b77-0672-454d-899a-8f0d3576b8f1%2Fbc0ebe9d-f388-47fc-910c-8c5720e3a84c%2FIMG_0918.png?table=block&id=2f26ce27-8df4-4786-8a38-665268426901&t=2f26ce27-8df4-4786-8a38-665268426901&width=200&cache=v2', // 显示页脚右侧的图片，通常放二维码
+  COMMERCE_FOOTER_RIGHT_TEXT: 'Survival Guide QR code', // 页脚右侧图片下的文字描述
 
-  COMMERCE_HOME_POSTS_COUNT: 9, // 首页展示商品数
-  COMMERCE_CONTACT_WHATSAPP_SHOW: true, // 是否展示whatsapp联系按钮 请配置 CONTACT_WHATSAPP
-  COMMERCE_CONTACT_TELEGRAM_SHOW: true, // 联系栏展示telegram按钮 请配置 CONTACT_TELEGRAM
+  COMMERCE_HOME_POSTS_COUNT: 18, // 首页展示商品数
+  COMMERCE_CONTACT_WHATSAPP_SHOW: false, // 是否展示whatsapp联系按钮 请配置 CONTACT_WHATSAPP
+  COMMERCE_CONTACT_TELEGRAM_SHOW: false, // 联系栏展示telegram按钮 请配置 CONTACT_TELEGRAM
 
   COMMERCE_ARTICLE_COPYRIGHT: true, // 文章版权声明
-  COMMERCE_ARTICLE_NOT_BY_AI: false // 显示非AI写作
+  COMMERCE_ARTICLE_NOT_BY_AI: true // 显示非AI写作
 }
 export default CONFIG
